1b6a2428351a232688a5eb90e7d7cab11fb3f756
Irfuirfu <116268495+Irfuirfu@users.noreply.github.com>
2023-07-12 17:46:31 +0530

    day 03 fnished

---
+++ src/main/java/Day03/practice/App.java
@@ -0,0 +1,19 @@
+package Day03.practice;
+public class App {
+public static void main(String[] args) {
+	 User user1 = new User();
+	 user1.setName("Irfu");
+     user1.setEmail("irfu@gmail.com");
+     user1.setId(125);
+     user1.setPassword("P@ssw0rd");
+ 	System.out.println("name=" + user1.getName() + ", email=" + user1.getEmail() + ", password=" + user1.getPassword()+ ", id=" + user1.getId());
+ 	
+ 	 User user2 = new User("irfan", "irfu@gmail.com", "A@dmin12");
+     user2.setId(126);
+ 	 System.out.println("name=" + user2.getName() + ", email=" + user2.getEmail() + ", password=" + user2.getPassword()+ ", id=" + user2.getId());
+ 	
+}
+}
+
+
+
