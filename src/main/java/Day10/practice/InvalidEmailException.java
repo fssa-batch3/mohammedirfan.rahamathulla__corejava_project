4928d4e7b815f40857c75d9576b1e611f21f44c2
Irfuirfu <116268495+Irfuirfu@users.noreply.github.com>
2023-07-21 19:08:39 +0530

    day10 finished

---
+++ src/main/java/Day10/practice/InvalidEmailException.java
@@ -0,0 +1,22 @@
+package Day10.practice;
+
+	public class InvalidEmailException extends Exception {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -8105491977357554060L;
+
+		// Calling each super constructors for each of the types
+		public InvalidEmailException(String msg) {
+			super(msg);
+		}
+
+		public InvalidEmailException(Throwable te) {
+			super(te);
+		}
+
+		public InvalidEmailException(String msg, Throwable te) {
+			super(msg, te);
+		}
+	}
+
